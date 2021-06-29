e0097652b6b5a9ea6f7599e6012f6c820f04fca5
Rian <rahmadiyan.muhammad1@gmail.com>
2021-06-29 18:29:53 +0700

    first commit

---
+++ Chad/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Chad
+//
+//  Created by Rian on 29/06/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
